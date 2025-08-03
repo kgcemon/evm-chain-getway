74d10120aa98a36c7176b55265a9b38cf4a91fb0
kgcemon <kgcemonbd@gmail.com>
2025-08-03 22:21:30 +0600

    added

--- app/Http/Controllers/api/Client/ClientWithdrawController.php
+++ app/Http/Controllers/api/Client/ClientWithdrawController.php
@@ -83,7 +83,7 @@ class ClientWithdrawController extends Controller
                 'user_id'    => $user->id,
                 'chain_id'   => $chain->id,
                 'amount'     => (float) ($responseData['amount'] ?? 0),
-                'trx_hash'   => $trxHash,
+                'trx_hash'   => $type == 'native' ? $trxHash : $ress->txHash,
                 'type'       => $type,
                 'token_name' => $type == 'token' ? $token->token_name : $chain->chain_name,
                 'status'     => ($responseData['status'] ?? false) ? 1 : 0,
