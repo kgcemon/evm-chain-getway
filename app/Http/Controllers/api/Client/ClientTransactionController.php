493042350faab1d1d0850d7d07cc2682110f2c7e
kgcemon <kgcemonbd@gmail.com>
2025-08-04 16:15:23 +0600

    updated

--- app/Http/Controllers/api/Client/ClientTransactionController.php
+++ app/Http/Controllers/api/Client/ClientTransactionController.php
@@ -10,7 +10,7 @@ class ClientTransactionController extends Controller
 {
     public function index(Request $request){
         $user = $request->user();
-        $transactions = Transactions::where('user_id', $user->id)->orderBy('created_at', 'desc')->paginate(20);
+        $transactions = Transactions::where('user_id', $user->id)->orderBy('created_at', 'desc')->orderBy('id','desc')->paginate(20);
         if(!$transactions->count() > 0){
             return response()->json([
                 'status' => false,
