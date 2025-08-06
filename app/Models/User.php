702d40fa5bb25cc9e56a4ba58c39a61a008320df
kgcemon <kgcemonbd@gmail.com>
2025-08-06 17:12:21 +0600

    updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'password',
         'wallet_address',
         'two_factor_secret',
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->string('wallet_address');
             $table->longText('two_factor_secret');
             $table->timestamp('email_verified_at')->nullable();
