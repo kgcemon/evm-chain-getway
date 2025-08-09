76c515f1c3bdbd1165873a8a326751e9bb973615
kgcemon <kgcemonbd@gmail.com>
2025-08-09 17:59:54 +0600

    updated

--- app/Services/TokenManage.php
+++ app/Services/TokenManage.php
@@ -109,7 +109,6 @@ class TokenManage extends Crypto
         }
 
         $nonce = (int)$this->getNonce($rpcUrl, $adminAddress);
-        dd(dechex($nonce));
         $gasLimit = 80000;
         $gasPrice = 1000000000;
 
