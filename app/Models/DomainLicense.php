5450fb2a55833a2e20582f2702552d90f282e02c
kgcemon <kgcemonbd@gmail.com>
2025-09-17 17:51:40 +0600

    updated

--- app/Models/DomainLicense.php
+++ app/Models/DomainLicense.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class DomainLicense extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'domain_license';
+
+    protected $fillable = [
+        'user_id',
+        'package_id',
+        'domain',
+        'license_key',
+        'register_at',
+        'expires_at',
+    ];
+
+    // Auto license generate
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($license) {
+            if (empty($license->license_key)) {
+                $license->license_key = self::generateLicenseKey();
+            }
+        });
+    }
+
+    public static function generateLicenseKey(int $length = 25): string
+    {
+        do {
+            $key = strtoupper(Str::random($length));
+            $key = substr(chunk_split($key, 5, '-'), 0, -1);
+        } while (self::where('license_key', $key)->exists());
+
+        return $key;
+    }
 }
--- database/migrations/2025_07_26_100058_create_domain_license_table.php
+++ database/migrations/2025_07_26_100058_create_domain_license_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\DomainLicense;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -22,6 +24,18 @@ return new class extends Migration
             $table->dateTime('expires_at');
             $table->timestamps();
         });
+    }
+
+    public function generateLicenseKey(int $length = 25): string
+    {
+        do {
+            $key = strtoupper(Str::random($length));
+
+            $key = substr(chunk_split($key, 5, '-'), 0, -1);
+
+        } while (DomainLicense::where('license_key', $key)->exists());
+
+        return $key;
     }
 
     /**
